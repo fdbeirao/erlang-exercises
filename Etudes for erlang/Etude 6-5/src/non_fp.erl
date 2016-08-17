99092d51a8ed9c51326ef6ee927cb104f05d41be
Fábio Beirão <fdbeirao@gmail.com>
2016-08-17 16:28:09 +0200

    FInalize implementation of Étude 6-5

--- Etudes for erlang/Etude 6-5/src/non_fp.erl
+++ Etudes for erlang/Etude 6-5/src/non_fp.erl
@@ -1,11 +1,17 @@
 -module(non_fp).
 
-%-export([generate_teeth/3]).
--compile(export_all).
+-export([generate_teeth/2]).
 
-%generate_teeth() ->
+generate_teeth(Teeth, ProbGood) ->
+  generate_teeth(Teeth, ProbGood, []).
     
-
+generate_teeth([], _, Teeth) -> 
+  lists:reverse(Teeth);
+generate_teeth([$F|Rest], ProbGood, Teeth) -> 
+  generate_teeth(Rest, ProbGood, [[0]|Teeth]);
+generate_teeth([$T|Rest], ProbGood, Teeth) ->
+  Tooth = generate_tooth(ProbGood), 
+  generate_teeth(Rest, ProbGood, [Tooth|Teeth]).
 
 generate_tooth(ProbGood) ->
     [getReading(ProbGood) rem 5 || _ <- lists:seq(1,6)].
